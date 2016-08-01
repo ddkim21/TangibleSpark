ac067adce950e503becd5a38aa00e2bbd38ceaa1
Michael Horn <michael-horn@northwestern.edu>
2016-08-01 08:00:00 -0500

    yay! smoothing.

--- web/dart/component.dart
+++ web/dart/component.dart
@@ -11,9 +11,10 @@ part of TangibleSpark;
  */
 class Component {
 
+
   /** Unique component ID */
   String id = "";
-   
+
   /** Name of the statement (e.g. resistor) */
   String name = 'resistor';
 
@@ -25,11 +26,10 @@ class Component {
   Connector leftJoint;
   Connector rightJoint;
 
-
   /** Is this component visible to the camera? */
   bool visible = false;
 
-   
+
   Component(Map definition) {
     id = definition['id'];
     name = definition['name'];
@@ -53,18 +53,32 @@ class Component {
 
     visible = false;
 
+    bool foundLeft = false;
+    bool foundRight = false;
+
 
     for (TopCode top in codes) {
       if (top.code == leftCode.code) {
-        _initLocation(top, leftCode, rightCode, 2.8);
+        _initLocation(top, leftCode, rightCode);
+        foundLeft = true;
       }
       else if (top.code == rightCode.code) {
-        _initLocation(top, rightCode, leftCode, -2.8);
+        _initLocation(top, rightCode, leftCode);
+        foundRight = true;
       }
     }
 
+    // if we didnt' find both codes, we estimate the position of 
+    // the opposite code based on the position of the first code
+    if (foundLeft && !foundRight) {
+      _copyTo(leftCode, rightCode, 2.8);
+    }
+    else if (foundRight && !foundLeft) {
+      _copyTo(rightCode, leftCode, -2.8);
+    }
+
     // fine tune orientations of the left and right code 
-    {
+    if (foundLeft || foundRight) {
       num between = rightCode.angleBetween(leftCode);
       leftCode.orientation = between;
       rightCode.orientation = between;
@@ -102,20 +116,34 @@ class Component {
   }
 
 
-  void _initLocation(TopCode found, TopCode match, TopCode opposite, num dx) {
-    match.copyFrom(found);
-    if (!visible) {
-      opposite.x = match.targetX(dx, 0);
-      opposite.y = match.targetY(dx, 0);
-      opposite.orientation = match.orientation;
-      opposite.unit = match.unit;
-    }
+  void _initLocation(TopCode found, TopCode match, TopCode opposite) {
+
+    // copy pose from the found topcode -- smoothing to prevent jitters
+    // the unit size is unlikely to change much with a fixed camera position
+    // so we weight that heavily in favor of the average from past frames
+    match.unit = match.unit * 0.9 + found.unit * 0.1;
+
+    // the orientation gets smoothed later by finding the angle between the left and right codes
+    match.orientation = found.orientation;
+
+    // the position is pretty jittery, but we can't assume that it will 
+    // stay as fixed as the unit
+    match.x = match.x * 0.5 + found.x * 0.5;
+    match.y = match.y * 0.5 + found.y * 0.5;
 
     // we've found at least one topcode, so this component is visible
     visible = true; 
 
     // mark the original topcode so that we don't use it again
     found.code = -1;
+  }
+
+
+  void _copyTo(TopCode src, TopCode opposite, num dx) {
+    opposite.x = src.targetX(dx, 0);
+    opposite.y = src.targetY(dx, 0);
+    opposite.orientation = src.orientation;
+    opposite.unit = src.unit;
   }
 
   
